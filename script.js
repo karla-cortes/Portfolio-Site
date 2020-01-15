0a652e77e7e4a09ac1386357be6b98a1af7e1497
karla-cortes <kmcortes7@gmail.com>
2020-01-14 22:02:15 -0500

    Updated mobile menu

--- script.js
+++ script.js
@@ -2,23 +2,43 @@ let menu = document.getElementById("js-menu");
 let navContainer = document.getElementById("nav");
 let buttonActive = document.getElementById('button');
 let logo = document.getElementsByClassName('logo-image')[0];
+let navItems = document.getElementsByClassName('nav-links');
+let navLinks = [];
 
-buttonActive.addEventListener('click', function(e) { 
+toggleClasses = () => {
+  buttonActive.classList.toggle('active');
+  menu.classList.toggle('active');
+  navContainer.classList.toggle('active'); 
+  logo.classList.toggle('active'); 
+}
+
+for (let i = 0 ; i < navItems.length; i ++) {
+  navLinks.push(navItems[i]);
+}
+
+buttonActive.addEventListener('click', (e) => { 
     e.preventDefault();
-    logo.classList.toggle('active');
-    buttonActive.classList.toggle('active');
-    menu.classList.toggle('active');
-    navContainer.classList.toggle('active');   
-  });   
+    toggleClasses();
+    navLinkClicked();
+});
   
-  // document.addEventListener('DOMContentLoaded', setTimeout(function(){
-  //   document.getElementById("overlay").style.display = 'none'; 
-  //  }, 3300
-  // ));
 
 
-  document.addEventListener("DOMContentLoaded", function(event) { 
-    setTimeout(function(){
-      document.getElementById("overlay").style.display = 'none'; 
-     }, 5000
-    )});
+function navLinkClicked() {
+  navLinks.forEach((ele) => {
+    ele.addEventListener('click', (e) =>{
+      logo.classList.remove('active');
+      menu.classList.remove('active');
+      buttonActive.classList.remove('active');
+      navContainer.classList.remove('active');
+    });
+  })
+}
+
+ 
+  
+document.addEventListener("DOMContentLoaded", (event) => { 
+  setTimeout(() =>{
+    document.getElementById("overlay").style.display = 'none'; 
+    }, 5000
+  )});

2ad19300648084bbfb7f23f7ccda121b088411ac
karla-cortes <kmcortes7@gmail.com>
2020-01-14 22:10:01 -0500

    Javascript Organization

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// Variables
 let menu = document.getElementById("js-menu");
 let navContainer = document.getElementById("nav");
 let buttonActive = document.getElementById('button');
 let logo = document.getElementsByClassName('logo-image')[0];
 let navItems = document.getElementsByClassName('nav-links');
 let navLinks = [];
+
+for (let i = 0 ; i < navItems.length; i ++) {
+  navLinks.push(navItems[i]);
+}
 
 toggleClasses = () => {
   buttonActive.classList.toggle('active');
@@ -12,17 +17,12 @@ toggleClasses = () => {
   logo.classList.toggle('active'); 
 }
 
-for (let i = 0 ; i < navItems.length; i ++) {
-  navLinks.push(navItems[i]);
-}
-
+// Mobile Menu
 buttonActive.addEventListener('click', (e) => { 
     e.preventDefault();
     toggleClasses();
     navLinkClicked();
 });
-  
-
 
 function navLinkClicked() {
   navLinks.forEach((ele) => {
@@ -35,8 +35,7 @@ function navLinkClicked() {
   })
 }
 
- 
-  
+// Loader
 document.addEventListener("DOMContentLoaded", (event) => { 
   setTimeout(() =>{
     document.getElementById("overlay").style.display = 'none'; 
